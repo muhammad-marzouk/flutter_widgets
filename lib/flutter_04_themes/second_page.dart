b5cb902ad28abfdf3a0cdab6758fedf7f45811cb
m.marzouk <muhammad.marzouk@gmail.com>
2023-11-26 20:31:22 +0200

    26-11-2023

--- lib/flutter_04_themes/second_page.dart
+++ lib/flutter_04_themes/second_page.dart
@@ -1,22 +1,14 @@
 import 'package:flutter/material.dart';
 
 class SecondPage extends StatelessWidget {
-  const SecondPage({super.key });
+  const SecondPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-
-        title: const Text("Second page"),
-      ),
-      body: const Center(
-        child:  Text('Second page',style: TextStyle(fontSize: 30),),
+    return const Scaffold(
+      body: Center(
+        child: Text('Second page'),
       ),
     );
   }
 }
-
-
-
