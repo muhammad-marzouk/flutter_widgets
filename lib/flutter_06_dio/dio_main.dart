574977b6c2c9a5029811b9ded302037f89e9540b
m.marzouk <muhammad.marzouk@gmail.com>
2023-11-27 21:01:48 +0200

    27-11-2023

--- lib/flutter_06_dio/dio_main.dart
+++ lib/flutter_06_dio/dio_main.dart
@@ -6,7 +6,6 @@ void main() async{
   if (kDebugMode) {
      print(responses.data.length);
     }
-
     for (int i=0; i<=responses.data.length-1;i++){
       if (kDebugMode) {
         print(responses.data[i]['name']);
