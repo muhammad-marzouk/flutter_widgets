fe3b605fe81d2070b88a9b83edc2f10ceb3f4fcd
m.marzouk <muhammad.marzouk@gmail.com>
2023-12-03 11:40:31 +0200

    3-120-2023

--- lib/flutter_01_widgets/widget_09_scaffold/widget_09_scaffold.dart
+++ lib/flutter_01_widgets/widget_09_scaffold/widget_09_scaffold.dart
@@ -1,122 +1,49 @@
 import 'package:flutter/material.dart';
-void main() => runApp(const MyApp());
 
+void main() {
+  runApp(MyApp());
+}
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: ScaffoldExample(),
-    );
-  }
-}
-
-class ScaffoldExample extends StatefulWidget {
-  const ScaffoldExample({super.key});
-  @override
-  State<ScaffoldExample> createState() => _ScaffoldExampleState();
-}
-
-class _ScaffoldExampleState extends State<ScaffoldExample> {
-  int _selectedIndex = 0;
-  static const TextStyle optionStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Index 0: Home',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 1: Business',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 2: School',
-      style: optionStyle,
-    ),
-  ];
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        elevation: 10.0,
-        title: const Text('muhammad'),
-        shadowColor: Colors.grey,
-      ),
-      body: Center(child: _widgetOptions[_selectedIndex]),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showModalBottomSheet<void>(
-            context: context,
-            builder: (BuildContext context) {
-              return SizedBox(
-                height: 100,
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      const Text('Modal BottomSheet'),
-                      ElevatedButton(
-                        child: const Text('Close BottomSheet'),
-                        onPressed: () => Navigator.pop(context),
-                      ),
-                    ],
-                  ),
-                ),
-              );
-            },
-          );
-        },
-        focusElevation: 5,
-        highlightElevation: 10,
-        backgroundColor: Colors.blue,
-        foregroundColor: Colors.white,
-        heroTag: 'MyFirstPage',
-        shape: const CircleBorder(),
-        child: const Icon(Icons.navigation),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-      bottomNavigationBar: Container(
-        decoration: const BoxDecoration(
-          boxShadow: <BoxShadow>[
-            BoxShadow(
-              color: Colors.black,
-              blurRadius: 10,
+    return MaterialApp(
+      home: Scaffold(
+        body: Stack(
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/images/1.png'),
+                    fit: BoxFit.cover),
+              ),
             ),
+            Align(
+                alignment: Alignment.bottomCenter,
+                child: Theme(
+                    data: Theme.of(context)
+                        .copyWith(canvasColor: Colors.transparent),
+                    child: BottomNavigationBar(
+                      currentIndex: 0,
+                      items: [
+                        BottomNavigationBarItem(
+                          icon: Icon(Icons.home),
+                          label: 'Home',
+                          // backgroundColor: Colors.white,
+                        ),
+                        BottomNavigationBarItem(
+                          icon: Icon(Icons.business),
+                          label: 'Business',
+                        ),
+                        BottomNavigationBarItem(
+                          icon: Icon(Icons.school),
+                          label: 'School',
+                        ),
+                      ],
+                    ))),
           ],
         ),
-        child: BottomNavigationBar(
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.business),
-              label: 'Business',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.school),
-              label: 'School',
-            ),
-          ],
-          elevation: 50.0,
-          iconSize: 30.0,
-          backgroundColor: Colors.blue[100],
-          currentIndex: _selectedIndex,
-          selectedItemColor: Colors.blue,
-          selectedLabelStyle: const TextStyle(color: Colors.black,fontSize: 16,fontWeight: FontWeight.bold,),
-          unselectedItemColor: Colors.grey,
-          onTap: (int index){
-            setState(() {
-              _selectedIndex = index;
-            });
-          },
-        ),
       ),
-      backgroundColor: Colors.white,
     );
   }
 }
